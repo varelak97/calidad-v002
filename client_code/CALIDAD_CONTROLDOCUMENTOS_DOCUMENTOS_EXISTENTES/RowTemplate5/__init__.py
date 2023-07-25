a97f4aee216590a3857ad1e173c444d2ddb52a99
erp@ensel.org <erp@ensel.org>
2023-07-25 13:07:32 +0000

    Edited CALIDAD_CONTROLDOCUMENTOS_DOCUMENTOS_EXISTENTES.RowTemplate5

--- client_code/CALIDAD_CONTROLDOCUMENTOS_DOCUMENTOS_EXISTENTES/RowTemplate5/__init__.py
+++ client_code/CALIDAD_CONTROLDOCUMENTOS_DOCUMENTOS_EXISTENTES/RowTemplate5/__init__.py
@@ -12,6 +12,15 @@ class RowTemplate5(RowTemplate5Template):
     self.init_components(**properties)
 
   def button_opciones_click(self, **event_args):
+    id_usuario_erp = self.parent.parent.parent.parent.parent.datos['id_usuario_erp']
+    botones = [
+        ("ACCEDER AL DOCUMENTO", "CALIDAD_CONTROLDOCUMENTOS_VISOR_GOOGLE_APP"),
+        ("HISTORIAL DE MODIFICACIONES", "CALIDAD_CONTROLDOCUMENTOS_HISTORIAL")
+      ]
+    if self.label_status.text == "LIBERADO":
+      #AQUÍ VOY
+      pass
+      
     evento = alert(
       content = "",
       title = "ELIGE UNA OPERACIÓN",
@@ -22,7 +31,7 @@ class RowTemplate5(RowTemplate5Template):
     )
     if evento != None:
       datos = {
-        'id_usuario_erp': self.parent.parent.parent.parent.parent.datos['id_usuario_erp'],
+        'id_usuario_erp': id_usuario_erp,
         'clave_form': evento,
         'id_registro_documento': anvil.server.call('obtener_id_registro', self.label_nombre_documento.text)
       }

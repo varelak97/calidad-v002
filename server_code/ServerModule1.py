b09d60dce52050f1272394cfe5b2daac541e8ff4
erp@ensel.org <erp@ensel.org>
2023-07-27 20:36:32 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -8,7 +8,7 @@ import json
 import requests
 
 global url_google_script
-url_google_script = "https://script.google.com/macros/s/AKfycbz6BaykBlo2qC14J6bXiKXRJggrtJyq0jT8jZkj36Nnv2LrofIJum4vueF_PuhiI2Jk/exec"
+url_google_script = "https://script.google.com/macros/s/AKfycbzPvC-_ldhWY_b3_3gXb3_dhnSRP0seZJPNV88cj0JY7cQRrZEaJUBqx82o6HgAS3CX/exec"
 
 #--- SECCIÓN DE FUNCIONES PARA FORMULARIO DE MENÚ PRINCIPAL ---
 @anvil.server.callable

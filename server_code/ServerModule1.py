0774969d85f7acee452e7e04528ac2f1f2b74f41
erp@ensel.org <erp@ensel.org>
2023-08-04 14:32:54 +0000

    Edited ServerModule1 and CALIDAD_CONTROLDOCUMENTOS_NUEVO_DOCUMENTO

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -168,7 +168,7 @@ def generar_documento(datos):
     'nivel': nuevo_renglon_registro_documento['nivel'],
     'folio': 'Folio: {folio}' if datos['folio'] else ''
   }
-  if datos['tipo_app'] in ("HOJA DE CÁLCULO","PRESENTACIÓN"):
+  if datos['tipo_app'] == "HOJA DE CÁLCULO":
     dicc_google_script['cantidad_hojas'] = datos['cantidad_hojas']
   dicc_google_script['emails_editores'] = obtener_emails_editores(nuevo_renglon_registro_documento['id_registro_documento'])
   dicc_google_script['emails_lectores'] = obtener_emails_lectores(dicc_google_script['emails_editores'])

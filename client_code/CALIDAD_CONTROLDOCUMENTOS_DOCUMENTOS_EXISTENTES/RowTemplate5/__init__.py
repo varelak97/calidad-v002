9a88011e6052047f1c04d637881b27c4be8191bd
erp@ensel.org <erp@ensel.org>
2024-06-09 00:51:18 +0000

    Edited CALIDAD_CONTROLDOCUMENTOS_DOCUMENTOS_EXISTENTES.RowTemplate5

--- client_code/CALIDAD_CONTROLDOCUMENTOS_DOCUMENTOS_EXISTENTES/RowTemplate5/__init__.py
+++ client_code/CALIDAD_CONTROLDOCUMENTOS_DOCUMENTOS_EXISTENTES/RowTemplate5/__init__.py
@@ -29,11 +29,33 @@ class RowTemplate5(RowTemplate5Template):
       buttons = botones
     )
     if evento != None:
-      self.parent.parent.parent.parent.visible = False
-      datos = {
-          'id_usuario_erp': id_usuario_erp,
-          'clave_form': evento,
-          'id_registro_documento': anvil.server.call('obtener_id_registro', self.label_nombre_documento.text)
-        }
-      self.parent.parent.parent.parent.parent.parent.raise_event('x-actualizar_form_activo', datos=datos)
-      self.parent.parent.parent.parent.visible = True
+      confirmacion = alert(
+        content = "¿Confirma que desea generar una nueva revisión de este documento?",
+        buttons=(("SI", True),("No", False))
+      )
+      if confirmacion:
+        self.parent.parent.parent.parent.visible = False
+        datos = {
+            'id_usuario_erp': id_usuario_erp,
+            'clave_form': evento,
+            'id_registro_documento': anvil.server.call('obtener_id_registro', self.label_nombre_documento.text)
+          }
+        """self.parent.parent.parent.parent.parent.parent.raise_event('x-actualizar_form_activo', datos=datos)
+        self.parent.parent.parent.parent.visible = True"""
+        with Notification("Trabajando en la generación del documento. Este proceso tomará algo de tiempo; por favor espera...", title="PROCESANDO PETICIÓN"):
+            self.background_task_google_script = anvil.server.call('lanzar_background_google_script', 'generacion_documento', self.datos)
+            tiempo_inicio = datetime.now()
+            tiempo_final = tiempo_inicio + timedelta(minutes=1, seconds=30)
+            ban_timeout = False
+            while self.background_task_google_script.is_running():
+              if datetime.now() >= tiempo_final:
+                ban_timeout = True
+                break
+              elif datetime.now() >= (tiempo_inicio + timedelta(seconds=2)):
+                respuesta = self.background_task_google_script.get_state()['respuesta']
+            #print(f"{self.background_task_google_script.get_error()}")
+            respuesta = self.background_task_google_script.get_state()['respuesta']
+          sleep(1)
+          print(f"Respuesta = {respuesta}")
+          if respuesta['exito_generacion_documento']:
+            Notification(f"El documento {self.datos['codigo']} ha sido generado satisfactoriamente.", title="¡ÉXITO!", style='success',timeout=4).show()

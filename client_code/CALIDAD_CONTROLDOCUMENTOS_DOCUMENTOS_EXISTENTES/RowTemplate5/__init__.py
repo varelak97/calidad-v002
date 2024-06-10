818a3205b74219a813b478e2c24175a577bc3f30
erp@ensel.org <erp@ensel.org>
2024-06-10 16:24:59 +0000

    Edited CALIDAD_CONTROLDOCUMENTOS_DOCUMENTOS_EXISTENTES.RowTemplate5

--- client_code/CALIDAD_CONTROLDOCUMENTOS_DOCUMENTOS_EXISTENTES/RowTemplate5/__init__.py
+++ client_code/CALIDAD_CONTROLDOCUMENTOS_DOCUMENTOS_EXISTENTES/RowTemplate5/__init__.py
@@ -23,7 +23,7 @@ class RowTemplate5(RowTemplate5Template):
     if self.label_status.text == "LIBERADO":
       ids_equipo = anvil.server.call('obtener_ids_equipo_de_documento', self.label_nombre_documento.text)
       if id_usuario_erp in ids_equipo:
-        botones.append(("NUEVA REVISIÓN", "CALIDAD_CONTROLDOCUMENTOS_NUEVO_DOCUMENTO"))
+        botones.append(("NUEVA REVISIÓN", "NUEVA REVISION"))
         pass
       
     evento = alert(
@@ -32,46 +32,56 @@ class RowTemplate5(RowTemplate5Template):
       buttons = botones
     )
     if evento != None:
-      confirmacion = alert(
-        content = "¿Confirma que desea generar una nueva revisión de este documento?",
-        buttons=(("SI", True),("No", False))
-      )
-      if confirmacion:
+      if evento == "NUEVA REVISION":
+        confirmacion = alert(
+          content = "¿Confirma que desea generar una nueva revisión de este documento?",
+          buttons=(("SI", True),("No", False))
+        )
+        if confirmacion:
+          self.parent.parent.parent.parent.visible = False
+          datos = {
+            'id_usuario_registrador': id_usuario_erp,
+            'marca_temporal': datetime.now(),
+            'id_registro_documento': anvil.server.call('obtener_id_registro', self.label_nombre_documento.text)
+          }
+          with Notification("Trabajando en la generación del documento. Este proceso tomará algo de tiempo; por favor espera...", title="PROCESANDO PETICIÓN"):
+            print(f"lo que se envia al servidor:{datos}")
+            self.background_task_google_script = anvil.server.call('lanzar_background_google_script', 'generar_nueva_revision', datos)
+            tiempo_inicio = datetime.now()
+            tiempo_final = tiempo_inicio + timedelta(minutes=1, seconds=30)
+            ban_timeout = False
+            while self.background_task_google_script.is_running():
+              if datetime.now() >= tiempo_final:
+                ban_timeout = True
+                break
+              elif datetime.now() >= (tiempo_inicio + timedelta(seconds=2)):
+                respuesta = self.background_task_google_script.get_state()['respuesta']
+            #print(f"{self.background_task_google_script.get_error()}")
+          respuesta = self.background_task_google_script.get_state()['respuesta']
+          sleep(1)
+          print(f"Respuesta = {respuesta}")
+          if respuesta['exito_creacion_nueva_revision']:
+            Notification(f"El documento {self.datos['codigo']} ha sido generado satisfactoriamente.", title="¡ÉXITO!", style='success',timeout=4).show()
+            Notification("Por favor espera...", title="REDIRIGIENDO")
+            datos = {
+              'id_usuario_erp': id_usuario_erp,
+              'clave_form': 'CALIDAD_CONTROLDOCUMENTOS_VISOR_GOOGLE_APP',
+              'id_registro_documento': respuesta['id_registro_documento']
+            }
+            self.parent.parent.parent.parent.parent.parent.raise_event('x-actualizar_form_activo', datos=datos)
+            self.parent.parent.parent.parent.visible = True
+          else:
+              alert(
+                content = respuesta['error'] + f"\n\nConfirma que tu dispositivo (PC, laptop o tablet) esté conectado a una red con acceso estable a internet e inténtalo nuevamente. Si el problema persiste, contacta al departamento de Sistemas.",
+                title = "OCURRIÓ UN ERROR",
+                dismissible=False
+              )
+      else:
         self.parent.parent.parent.parent.visible = False
         datos = {
-          'id_usuario_registrador': id_usuario_erp,
-          'marca_temporal': datetime.now(),
-          'id_registro_documento': anvil.server.call('obtener_id_registro', self.label_nombre_documento.text)
-        }
-        with Notification("Trabajando en la generación del documento. Este proceso tomará algo de tiempo; por favor espera...", title="PROCESANDO PETICIÓN"):
-          print(f"lo que se envia al servidor:{datos}")
-          self.background_task_google_script = anvil.server.call('lanzar_background_google_script', 'generar_nueva_revision', datos)
-          tiempo_inicio = datetime.now()
-          tiempo_final = tiempo_inicio + timedelta(minutes=1, seconds=30)
-          ban_timeout = False
-          while self.background_task_google_script.is_running():
-            if datetime.now() >= tiempo_final:
-              ban_timeout = True
-              break
-            elif datetime.now() >= (tiempo_inicio + timedelta(seconds=2)):
-              respuesta = self.background_task_google_script.get_state()['respuesta']
-          #print(f"{self.background_task_google_script.get_error()}")
-        respuesta = self.background_task_google_script.get_state()['respuesta']
-        sleep(1)
-        print(f"Respuesta = {respuesta}")
-        if respuesta['exito_creacion_nueva_revision']:
-          Notification(f"El documento {self.datos['codigo']} ha sido generado satisfactoriamente.", title="¡ÉXITO!", style='success',timeout=4).show()
-          Notification("Por favor espera...", title="REDIRIGIENDO")
-          datos = {
             'id_usuario_erp': id_usuario_erp,
-            'clave_form': 'CALIDAD_CONTROLDOCUMENTOS_VISOR_GOOGLE_APP',
-            'id_registro_documento': respuesta['id_registro_documento']
+            'clave_form': evento,
+            'id_registro_documento': anvil.server.call('obtener_id_registro', self.label_nombre_documento.text)
           }
-          self.parent.parent.parent.parent.parent.parent.raise_event('x-actualizar_form_activo', datos=datos)
-          self.parent.parent.parent.parent.visible = True
-        else:
-            alert(
-              content = respuesta['error'] + f"\n\nConfirma que tu dispositivo (PC, laptop o tablet) esté conectado a una red con acceso estable a internet e inténtalo nuevamente. Si el problema persiste, contacta al departamento de Sistemas.",
-              title = "OCURRIÓ UN ERROR",
-              dismissible=False
-            )
+        self.parent.parent.parent.parent.parent.parent.raise_event('x-actualizar_form_activo', datos=datos)
+        self.parent.parent.parent.parent.visible = True

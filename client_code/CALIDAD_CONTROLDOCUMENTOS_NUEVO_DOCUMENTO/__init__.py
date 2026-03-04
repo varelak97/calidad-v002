755c4deaeab3d3f7ebddfe4612081a01e07f652c
erp@ensel.org <erp@ensel.org>
2026-03-04 17:31:33 +0000

    Edited CALIDAD_CONTROLDOCUMENTOS_NUEVO_DOCUMENTO

--- client_code/CALIDAD_CONTROLDOCUMENTOS_NUEVO_DOCUMENTO/__init__.py
+++ client_code/CALIDAD_CONTROLDOCUMENTOS_NUEVO_DOCUMENTO/__init__.py
@@ -221,7 +221,7 @@ class CALIDAD_CONTROLDOCUMENTOS_NUEVO_DOCUMENTO(CALIDAD_CONTROLDOCUMENTOS_NUEVO_
           for k,v in self.datos.items():
             print(k,":",v)
           
-          with Notification("Trabajando en la generación del documento. Este proceso tomará algo de tiempo; por favor espera...", title="PROCESANDO PETICIÓN"):
+          """"with Notification("Trabajando en la generación del documento. Este proceso tomará algo de tiempo; por favor espera...", title="PROCESANDO PETICIÓN"):
             self.background_task_google_script = anvil.server.call('lanzar_background_google_script', 'generacion_documento', self.datos)
             tiempo_inicio = datetime.now()
             tiempo_final = tiempo_inicio + timedelta(minutes=1, seconds=30)
@@ -271,7 +271,8 @@ class CALIDAD_CONTROLDOCUMENTOS_NUEVO_DOCUMENTO(CALIDAD_CONTROLDOCUMENTOS_NUEVO_
               content = respuesta['error'] + f"\n\nConfirma que tu dispositivo (PC, laptop o tablet) esté conectado a una red con acceso estable a internet e inténtalo nuevamente. Si el problema persiste, contacta al departamento de Sistemas.",
               title = "OCURRIÓ UN ERROR",
               dismissible=False
-            )
+            )"""
+          #end pruebas de lo que envia anvil a appscript
     self.content_panel.visible = True
 
   def drop_down_tipo_archivo_change(self, **event_args):
